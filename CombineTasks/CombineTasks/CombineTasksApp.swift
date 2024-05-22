5dbb5a34f3e85e2b74928da4bdaaec840024b96e
devinigor <devinigor@icloud.com>
2024-05-22 10:29:42 +0300

    app init

---
+++ CombineTasks/CombineTasks/CombineTasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineTasksApp.swift
+//  CombineTasks
+//
+//  Created by Игорь Девин on 22.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct CombineTasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
