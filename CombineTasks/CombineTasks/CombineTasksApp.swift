341321da4b1da448c8d8a7777122759d67b46cb3
devinigor <devinigor@icloud.com>
2024-05-27 12:05:23 +0300

    timer done

--- CombineTasks/CombineTasks/CombineTasksApp.swift
+++ CombineTasks/CombineTasks/CombineTasksApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct CombaineTasksApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ProductLoadingView()
         }
     }
 }
--- CombineTasks/CombineTasks/ContentView.swift
+++ CombineTasks/CombineTasks/ContentView.swift
@@ -7,20 +7,80 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+ struct ProductLoadingView: View {
 
-#Preview {
-    ContentView()
-}
+     var body: some View {
+         VStack {
+             switch viewModel.state {
+             case .loading:
+                 timerView
+                 if viewModel.isConnecting {
+                     loadingProduct
+                 } else {
+                     VStack {
+                         Spacer()
+                         buttonStartView
+                     }
+                 }
+             case .data:
+                 timerView
+                 listView
+                 buttonStartView
+             case .error(let error):
+                 Text(error.localizedDescription)
+             }
+         }
+     }
 
+     @StateObject var viewModel = ProductViewModel()
+
+     private var timerView: some View {
+         Text("time left \(viewModel.timerLeft)")
+             .font(.title)
+     }
+
+     private var listView: some View {
+         let filteredProducts = viewModel.filterByPrice(products: viewModel.dataToProducts)
+         let finalProducts = viewModel.filterByImagePresence(products: filteredProducts)
+
+         return List(finalProducts, id: \.self) { item in
+             HStack {
+                 Image(systemName: item.imageName ?? "")
+                 Text(item.name)
+                 Text("\(item.price) руб")
+             }
+         }
+     }
+
+     private var buttonStartView: some View {
+         Button(action: {
+             withAnimation(Animation.easeInOut(duration: 0.3).repeatCount(13, autoreverses: true)) {
+                 viewModel.start()
+                 viewModel.performAnimation()
+             }
+         }, label: {
+             Text("Start")
+                 .font(.body)
+                 .padding()
+                 .foregroundColor(.white)
+                 .background(RoundedRectangle(cornerRadius: 100).fill(.blue))
+                 .cornerRadius(8)
+                 .scaleEffect(viewModel.isAnimating ? 0.9 : 1)
+         })
+     }
+
+     private var loadingProduct: some View {
+         VStack {
+             if viewModel.isLoadingProduct {
+                 Text("Loading items...")
+                     .font(.body)
+                     .foregroundColor(.black)
+             }
+         }
+     }
+ }
+
+ #Preview {
+     ProductLoadingView()
+ }
 
